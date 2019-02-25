32a20cb33b796a0a88112bb5707db20c82024342
jpclark6 <jpclark6@gmail.com>
2019-02-25 09:32:31 -0700

    adds body to giphy service

--- app/services/giphy_service.rb
+++ app/services/giphy_service.rb
@@ -1,7 +1,23 @@
 class GiphyService
-  def initialize(city_state)
-    @city_state = city_state
+  def initialize(weather_description)
+    @weather_description = weather_description
   end
 
-  
+  def weather_gif
+    get_json("/v1/gifs/search", {q: weather_description})
+  end
+
+  private
+
+  def get_json(url, params)
+    response = conn.get(url, params)
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: 'https://api.giphy.com') do |faraday|
+      faraday.params[:api_key] = ENV['GIPHY_API_KEY']
+    end
+  end
 end
