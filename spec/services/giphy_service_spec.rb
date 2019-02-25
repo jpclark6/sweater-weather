05ae444cd22e095f6f587649ab676c40f1db3615
jpclark6 <jpclark6@gmail.com>
2019-02-25 09:26:49 -0700

    adds json return test for giphy service

--- spec/services/giphy_service_spec.rb
+++ spec/services/giphy_service_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 describe 'as a giphy service' do
   it 'exists' do
-    location = 'denver,co'
-    gif = GiphyService.new(location)
+    weather_description = "Partly cloudy throughout the day."
+    gif = GiphyService.new(weather_description)
     expect(gif).to be_instance_of(GiphyService)
+  end
+
+  it 'returns a json object when called for' do
+    weather_description = "Partly cloudy throughout the day."
+    gif = GiphyService.new(weather_description)
+    data = gif.weather_gif
+    expect(data).to be_instance_of(Hash)
+    expect(data[:data][0][:images][:original][:url]).to be_instance_of(String)
   end
 end
