6ba1245bd074f2f5edeed0ff0aa2cf2fd71f6242
jpclark6 <jpclark6@gmail.com>
2019-02-25 10:24:28 -0700

    all tests currently passing

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::GifsController < ApplicationController
   def index
-    binding.pry
     render json: WeatherGifSerializer.make_json(params[:location])
   end
 end
--- spec/facades/weather_gif_facade_spec.rb
+++ spec/facades/weather_gif_facade_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
 describe 'getting weather gifs based on location' do
-  it 'can build weather gif objects' do
+  it 'can build weather gif objects', :vcr do
     city_state = 'denver,co'
     weather_gif_by_day = WeatherGifFacade.new(city_state).build_days
-    expect(weather_gif_by_day).to be_instance_of(WeatherGif)
-    expect(weather_gif_by_day).to be_instance_of(WeatherGif)
-    expect(weather_gif_by_day).to be_instance_of(WeatherGif)
-    expect(weather_gif_by_day.gif).to include('.gif')
-    expect(weather_gif_by_day.time).to be_instance_of(Integer)
-    expect(weather_gif_by_day.summary).to be_instance_of(String)
-    expect(weather_gif_by_day.gif).to include('.gif')
-    expect(weather_gif_by_day.time).to be_instance_of(Integer)
-    expect(weather_gif_by_day.summary).to be_instance_of(String)
+    expect(weather_gif_by_day).to be_instance_of(Array)
+    expect(weather_gif_by_day[0][:url]).to include('.gif')
+    expect(weather_gif_by_day[0][:time]).to be_instance_of(Integer)
+    expect(weather_gif_by_day[0][:summary]).to be_instance_of(String)
+    expect(weather_gif_by_day[0][:url]).to include('.gif')
+    expect(weather_gif_by_day[0][:time]).to be_instance_of(Integer)
+    expect(weather_gif_by_day[0][:summary]).to be_instance_of(String)
   end
 end
