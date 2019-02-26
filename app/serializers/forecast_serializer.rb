30a83801aa4ed162089c8ede1e402f001491c328
jpclark6 <jpclark6@gmail.com>
2019-02-26 15:43:54 -0700

    delete is functional

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -4,6 +4,11 @@ class ForecastSerializer
     { data: {currently: forecast.current, hourly: forecast.hourly_weather, daily: forecast.daily_weather} }
   end
 
+  def self.make_local_only_json(city_state)
+    forecast = ForecastFacade.new(lat_lng(city_state)).weather
+    {currently: forecast.current}
+  end
+
   def self.lat_lng(city_state)
     GeocodeFacade.new(city_state).lat_lng
   end
---
+++ spec/api/v1/features/user_can_delete_favorites_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+describe 'as a user' do
+  it 'can delete favorites' do
+    email = 'example@example.com'
+    password = 'password'
+    api_key = 'abc123'
+    user = User.create(email: email, password: password, api_key: api_key)
+    post "/api/v1/favorites?api_key=#{api_key}&location=Denver, CO"
+    post "/api/v1/favorites?api_key=#{api_key}&location=Miami, FL"
+
+    delete "/api/v1/favorites?api_key=#{api_key}&location=Denver, CO"
+
+    expect(response).to be_successful
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data[:data].length).to eq(1)
+    expect(data[:data][0][:location]).to eq("Denver, CO")
+    expect(data[:data][0][:current_weather][:currently][:temperature]).to be_between(-10, 115).inclusive
+  end
+
+  it 'errors out if wrong api key' do
+    email = 'example@example.com'
+    password = 'password'
+    api_key = 'abc123'
+    user = User.create(email: email, password: password, api_key: api_key)
+    post "/api/v1/favorites?api_key=#{api_key}&location=Denver, CO"
+    post "/api/v1/favorites?api_key=#{api_key}&location=Miami, FL"
+
+    delete "/api/v1/favorites?api_key=#{api_key}x&location=Denver, CO"
+
+    expect(status).to eq(401)
+    data = JSON.parse(response.body, symbolize_names: true)
+    expect(data[:status]).to eq('Something went wrong')
+  end
+
+  it 'errors out if no api key' do
+    email = 'example@example.com'
+    password = 'password'
+    api_key = 'abc123'
+    user = User.create(email: email, password: password, api_key: api_key)
+    post "/api/v1/favorites?api_key=#{api_key}&location=Denver, CO"
+    post "/api/v1/favorites?api_key=#{api_key}&location=Miami, FL"
+
+    delete "/api/v1/favorites?location=Denver, CO"
+
+    expect(status).to eq(401)
+    data = JSON.parse(response.body, symbolize_names: true)
+    expect(data[:status]).to eq('Something went wrong')
+  end
+
+  it 'errors out if wrong city given' do
+    email = 'example@example.com'
+    password = 'password'
+    api_key = 'abc123'
+    user = User.create(email: email, password: password, api_key: api_key)
+    post "/api/v1/favorites?api_key=#{api_key}&location=Denver, CO"
+    post "/api/v1/favorites?api_key=#{api_key}&location=Miami, FL"
+
+    delete "/api/v1/favorites?api_key=#{api_key}x&location=Nowhere, CO"
+
+    expect(status).to eq(401)
+    data = JSON.parse(response.body, symbolize_names: true)
+    expect(data[:status]).to eq('Something went wrong')
+  end
+end
