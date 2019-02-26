a9f6c62d5e02c9eeb3dd0f1da5d9c74fed98ce6b
jpclark6 <jpclark6@gmail.com>
2019-02-26 09:24:27 -0700

    sad path user create finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  validates_presence_of :email
-  validates_presence_of :password, if: :password
+  validates_presence_of :email, :password
   validates_uniqueness_of :email, :api_key
   validates_confirmation_of :password
 
--- spec/api/v1/account_creation_spec.rb
+++ spec/api/v1/account_creation_spec.rb
@@ -12,4 +12,29 @@ describe 'to create a user' do
     data = JSON.parse(response.body, symbolize_names: true)
     expect(data[:api_key]).to eq(User.last.api_key)
   end
+  it 'does not create user when account already exists' do
+    email = "whatever@example.com"
+    password = "password"
+    password_confirmation = "password"
+    post "/api/v1/users?email=#{email}&password=#{password}&password_confirmation=#{password_confirmation}"
+
+    email = "whatever@example.com"
+    password = "password2"
+    password_confirmation = "password2"
+    post "/api/v1/users?email=#{email}&password=#{password}&password_confirmation=#{password_confirmation}"
+
+    expect(response).to be_successful
+    data = JSON.parse(response.body, symbolize_names: true)
+    expect(data[:status]).to eq("Account not created. Error occured.")
+  end
+  it 'does not create user when account password confirmation does not match' do
+    email = "whatever@example.com"
+    password = "password"
+    password_confirmation = "password2"
+    post "/api/v1/users?email=#{email}&password=#{password}&password_confirmation=#{password_confirmation}"
+
+    expect(response).to be_successful
+    data = JSON.parse(response.body, symbolize_names: true)
+    expect(data[:status]).to eq("Account not created. Error occured.")
+  end
 end
